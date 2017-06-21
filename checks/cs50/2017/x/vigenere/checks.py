13a045aeef446f4045a202f707f4928dac32ec20
hljames <haileyjames@college.harvard.edu>
2017-06-21 11:40:04 -0400

    updated test names

--- checks/cs50/2017/x/vigenere/checks.py
+++ checks/cs50/2017/x/vigenere/checks.py
@@ -17,41 +17,41 @@ class Vigenere(TestCase):
         self.spawn("clang -o vigenere vigenere.c -lcs50").exit(0)
 
     @check("compiles")
-    def test_aa(self):
+    def aa(self):
         """encrypts "a" as "a" using "a" as keyword"""
         self.spawn("./vigenere a").stdin("a").stdout("ciphertext:\s*a\n", "ciphertext: a").exit(0)
 
     @check("compiles")
-    def test_bazbarfoo(self):
+    def bazbarfoo_caqgon(self):
         """encrypts "barfoo" as "caqgon" using "baz" as keyword"""
         self.spawn("./vigenere baz").stdin("barfoo").stdout("ciphertext:\s*caqgon\n", "ciphertext: caqgon").exit(0)
 
     @check("compiles")
-    def test_mixedBaZBARFOO(self):
+    def mixedBaZBARFOO(self):
         """encrypts "BaRFoo" as "CaQGon" using "BaZ" as keyword"""
         self.spawn("./vigenere BaZ").stdin("BaRFoo").stdout("ciphertext:\s*CaQGon\n", "ciphertext: CaQGon").exit(0)
 
     @check("compiles")
-    def test_allcapsBAZBARFOO(self):
+    def allcapsBAZBARFOO(self):
         """encrypts "BARFOO" as "CAQGON" using "BAZ" as keyword"""
         self.spawn("./vigenere BAZ").stdin("BARFOO").stdout("ciphertext:\s*CAQGON\n", "ciphertext: CAQGON").exit(0)
 
     @check("compiles")
-    def test_bazworld(self):
+    def bazworld(self):
         """encrypts "world!$?" as "xoqmd!$?" using "baz" as keyword"""
         self.spawn("./vigenere baz").stdin("world!$?").stdout("ciphertext:\s*xoqmd!\$\?\n", "ciphertext: xoqmd!$?").exit(0)
 
     @check("compiles")
-    def test_noarg(self):
+    def noarg(self):
         """handles lack of argv[1]"""
         self.spawn("./vigenere").exit(1)
 
     @check("compiles")
-    def test_toomanyargs(self):
+    def toomanyargs(self):
         """handles argc > 2"""
         self.spawn("./vigenere 1 2 3").exit(1)
 
     @check("compiles")
-    def test_reject(self):
+    def reject(self):
         """rejects "Hax0r2" as keyword"""
         self.spawn("./vigenere Hax0r2").exit(1)
